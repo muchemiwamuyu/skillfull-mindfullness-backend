be03c49e0d17016b07045292cc2dc81fa7b74d79
muchemi wamuyu <muchemiedwin68@gmail.com>
2026-01-23 14:29:02 +0300

    docker database intergration

--- urbantrends_backend/settings.py
+++ urbantrends_backend/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -85,14 +86,9 @@ WSGI_APPLICATION = 'urbantrends_backend.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'urbantrends_backend',
-        'USER': 'postgres',
-        'PASSWORD': '5336',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL")
+    )
 }
 
 
