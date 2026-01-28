6632bfe64b97fc0722b8c33cc5525e40921b19c1
muchemi wamuyu <muchemiedwin68@gmail.com>
2026-01-28 20:46:15 +0300

    database resolve

--- urbantrends_backend/settings.py
+++ urbantrends_backend/settings.py
@@ -87,18 +87,18 @@ WSGI_APPLICATION = 'urbantrends_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": dj_database_url.config(
-#         default=os.getenv("DATABASE_URL")
-#     )
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",  # <-- file will persist here
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL")
+    )
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",  # <-- file will persist here
+#     }
+# }
 
 
 # DATABASES = {
