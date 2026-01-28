919154b853b50333853dd93bfe0a0d6cebb68a78
muchemi wamuyu <muchemiedwin68@gmail.com>
2026-01-28 20:43:38 +0300

    serializers update

--- dashboard_services/serializers.py
+++ dashboard_services/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 from .models import DashboardProject
 
-# serializers
 class DashboardProjectSerializer(serializers.ModelSerializer):
-    model = DashboardProject
-    fields = '__all__'
-    read_only_fields = ["id", "created_at", "project_by" "updated_at"]
+    project_by = serializers.StringRelatedField(read_only=True)
+    class Meta:
+        model = DashboardProject
+        fields = "__all__"
+        read_only_fields = ["id", "created_at", "project_by", "updated_at"]
