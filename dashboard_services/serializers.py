1dd945ee36965f6cfb624edbadc0e18ef25c3bc4
muchemi wamuyu <muchemiedwin68@gmail.com>
2026-02-05 10:58:02 +0300

    Dashboard teams endpoint update

--- dashboard_services/serializers.py
+++ dashboard_services/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import DashboardProject
+from .models import DashboardProject, DashboardTeams
 
 class DashboardProjectSerializer(serializers.ModelSerializer):
     project_by = serializers.StringRelatedField(read_only=True)
@@ -7,3 +7,11 @@ class DashboardProjectSerializer(serializers.ModelSerializer):
         model = DashboardProject
         fields = "__all__"
         read_only_fields = ["id", "created_at", "project_by", "updated_at"]
+
+class DashboardTeamsSerializer(serializers.ModelSerializer):
+    added_by = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = DashboardTeams
+        fields = "__all__"
+        read_only_fields = ["id", "created_at", "added_by", "updated_at"]
