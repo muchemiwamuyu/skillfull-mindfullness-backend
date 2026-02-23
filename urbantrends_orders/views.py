6a4cc819febba94c068cec85243fd2d55c71c128
muchemi wamuyu <muchemiedwin68@gmail.com>
2026-02-23 18:37:23 +0300

    stupid minor error

--- urbantrends_orders/views.py
+++ urbantrends_orders/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import generics
+from rest_framework.response import Response
 from .models import Order
 from .serializers import OrderCreateSerializer, OrderSerializer
 from rest_framework.permissions import IsAuthenticated
